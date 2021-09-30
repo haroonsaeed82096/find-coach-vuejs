9867f663ae1293e66475fb9abd02d4c5f1c52f2a
Haroon <haroon.saeed@outlook.com>
2021-09-30 21:17:01 +0200

    header and layout

--- src/router.js
+++ src/router.js
@@ -6,22 +6,25 @@ import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReceived from './pages/requests/RequestReceived.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', redirect: 'coaches'},
-        { path: '/coaches', component: CoachesList },
-        { path: '/coaches/id', component: CoachDetail },
-        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
-        { path: '/register', component: CoachRegistration },
-        { path: '/requests', component: RequestReceived },
-        { path: '/:notFound(.*)', component: NotFound },
-        
+      { path: '/', redirect: '/coaches' },
+      { path: '/coaches', component: CoachesList },
+      {
+        path: '/coaches/:id',
+        component: CoachDetail,
+        children: [
+          { path: 'contact', component: ContactCoach } // /coaches/c1/contact
+        ]
+      },
+      { path: '/register', component: CoachRegistration },
+      { path: '/requests', component: RequestsReceived },
+      { path: '/:notFound(.*)', component: NotFound }
     ]
-})
-
+  });
 export default router;
