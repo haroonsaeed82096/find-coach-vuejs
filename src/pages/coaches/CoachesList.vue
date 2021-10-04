332d41bde3a1a10f45aa1fa2b3f35d6eb0bb24e4
Haroon <haroon.saeed@outlook.com>
2021-10-04 12:42:55 +0200

    Display data in list

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,29 @@
 <template>
-  <section>Filter</section>
   <section>
     <div class="controls">
-      <button>Refresh</button>
+      <button @click="increment">Refresh</button>
       <router-link to="/register">Register as coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -26,3 +34,15 @@ export default {
   }
 };
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
